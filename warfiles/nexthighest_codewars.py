6b2d4efee53b43b0e431ae73ce1363829a329386
MadTown86 <101869180+MadTown86@users.noreply.github.com>
2022-09-16 05:10:14 -0500

    Home commit - nexthighest redo

--- warfiles/nexthighest_codewars.py
+++ warfiles/nexthighest_codewars.py
@@ -1,21 +1,53 @@
-# TEST NUMBER - 1964827
+# 1234567321
+# 15065330
+# 15065330
+import itertools
 
-beginnum = 0
-counter = 0
-def recurseit(num):
-    global counter
-    global beginnum
-    if counter == 0:
-        beginnum = num
-        counter += 1
+def next_bigger(num):
+    """
+    1. Test outlier conditions
+    2. Loop through digits from right to left, using sort to check if potential new highest
+    3. Loop only checks up to second highest place digit, create algorithm to check for next largest highest place value
+    """
+
+    local_num = str(num)
+
+    # Outlier cases - 2 digits
+    if len(local_num) == 2:
+        if local_num[:1] >= local_num[1:]:
+            return -1
+        else:
+            return int((local_num[1:] + local_num[:1]))
+
     else:
-        thisnum = str(num)
-        firsthalf = thisnum[:len(thisnum)-2]
-        toplevel_placecount = 2
-        secondhalf = thisnum[:-toplevel_placecount]
+        dcount_floop = 2
 
-        def innerrecurse(firsthalf, secondhalf):
-
+    while dcount_floop < len(local_num):
+        front = local_num[:-dcount_floop]
+        original_back = local_num[-dcount_floop:]
 
 
+        # This bit is not True for following setup: 67321 sorted returns 76---, need 71---
+        if "".join(sorted(original_back, reverse=True)) > original_back:
+            higher_listchunk = sorted(original_back, reverse=True)
+            higher_firstdigit = higher_listchunk.pop(0)
+            sort_higher_listchunk = sorted(higher_listchunk)
+            next_highest = "".join(itertools.chain(higher_firstdigit, sort_higher_listchunk))
+        else:
+            continue
 
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    # print(next_bigger(1234567908))
+    # print(next_bigger(59483726))
+    # print(next_bigger(9876789))
+    # print(next_bigger(1357642))
+    print(next_bigger2(890123683457))  # 890123683457
