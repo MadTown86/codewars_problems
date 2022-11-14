e0369592931cfa72a6b8f6605bd8dbc5a8a9ba7e
MadTown86 <101869180+MadTown86@users.noreply.github.com>
2022-11-14 05:46:06 -0600

    battleshipfieldvalidator.py

--- warfiles/analysis_logicstructures_codewars.py
+++ warfiles/analysis_logicstructures_codewars.py
@@ -34,14 +34,22 @@ def mix2(s1, s2):
 
 """Answers to land Perimter Question"""
 
+
+
+
+"""
+
 land = lambda a: sum(t == ('X', 'X') for r in a for t in zip(r, r[1:])) * 2
 
 def land_perimeter(a):
     return 'Total land perimeter: ' + str(''.join(a).count('X') * 4 - land(a) - land(zip(*a)))
+    
+"""
+
 """
 land_perimeter(["OXOOOX", "OXOXOO", "XXOOOX", "OXXXOO", "OOXOOX", "OXOOOO", "OOXOOX", "OOXOOO", "OXOOOO", "OXOOXX"]
 """
-
+"""
 def land_perimeter2(arr):
     I, J = len(arr), len(arr[0])
 
@@ -57,9 +65,10 @@ def land_perimeter2(arr):
     return 'Total land perimeter: ' + str(P)
 
 """
-The Next Bigger Most Liked Solutions
-"""
 
+# Next Bigger
+
+"""
 import itertools
 def next_bigger(n):
     s = list(str(n))
@@ -82,3 +91,151 @@ def next_bigger(n):
                 nums[i + 1:] = sorted(nums[i + 1:])
                 return int(''.join(nums))
     return -1
+
+"""
+
+# Battleship Problem
+"""
+def validateBattlefield(field):
+    n, m = len(field), len(field[0])
+    def cell(i, j):
+        if i < 0 or j < 0 or i >= n or j >= m: return 0
+        return field[i][j]
+    def find(i, j):
+        if cell(i + 1, j - 1) or cell(i + 1, j + 1): return 10086
+        if cell(i, j + 1) and cell(i + 1, j): return 10086
+        field[i][j] = 2
+        if cell(i, j + 1): return find(i, j + 1) + 1
+        if cell(i + 1, j): return find(i + 1, j) + 1
+        return 1
+    num = [0] * 5
+    for i in xrange(n):
+        for j in xrange(m):
+            if cell(i, j) == 1:
+                r = find(i, j)
+                if r > 4: return False
+                num[r] += 1
+    [tmp, submarines, destroyers, cruisers, battleship] = num
+    return battleship == 1 and cruisers == 2 and destroyers == 3 and submarines == 4
+"""
+
+"""
+from scipy.ndimage.measurements import label, find_objects, np
+def validate_battlefield(field):
+    field = np.array(field)
+    return sorted(
+        ship.size if min(ship.shape) == 1 else 0
+        for ship in (field[pos] for pos in find_objects(label(field, np.ones((3,3)))[0]))
+    ) == [1,1,1,1,2,2,2,3,3,4]
+"""
+
+# Nesting Structure Comparison
+
+"""
+def same_structure_as(original,other):
+    if isinstance(original, list) and isinstance(other, list) and len(original) == len(other):
+        for o1, o2 in zip(original, other):
+            if not same_structure_as(o1, o2): return False
+        else: return True
+    else: return not isinstance(original, list) and not isinstance(other, list)
+"""
+
+"""
+def same_structure_as(original, other):
+    if type(original) == list == type(other):
+        return len(original) == len(other) and all(map(same_structure_as, original, other))
+    else:
+        return type(original) != list != type(other)
+"""
+
+"""
+def same_structure_as(a, b):
+    return (False if not (isinstance(a, list) and isinstance(b, list)) or len(a) != len(b)
+            else all(same_structure_as(c, d) for c, d in zip(a, b) if isinstance(c, list)))
+"""
+
+"""
+s = same_structure_as = lambda a, b: type(a) == type(b) == list and len(a) == len(b) and all(map(s, a, b)) if type(a) == list else 1
+"""
+
+# Infix to Postfix
+
+"""
+def to_postfix(infix):
+    output = ""
+    stack = []
+    precedence = {"+": 1, "-": 1, "*": 2, "/": 2, "^": 3}
+    for c in infix:
+        if c.isalnum():
+            output += c
+        elif c == '(':
+             stack.append(c)
+        elif c == ')':
+            while stack[-1] != "(":
+                output += stack.pop()
+            stack.pop()
+        else:
+            while stack and stack[-1] != "(" and \
+                (precedence[stack[-1]] > precedence[c] or
+                    (precedence[stack[-1]] == precedence[c] and c != "^")):
+                output += stack.pop()
+            stack.append(c)
+    while stack:
+        output += stack.pop()
+    return output
+"""
+
+"""
+def to_postfix (infix):
+    for i in range(10):
+        infix = infix.replace(str(i), f"x({i})")
+    infix = infix.replace('^', '**')
+    class x:
+        def __init__(self, v): self.v = str(v)
+        def __add__(self, y): return x(self.v + y.v + '+')
+        def __sub__(self, y): return x(self.v + y.v + '-')
+        def __mul__(self, y): return x(self.v + y.v + '*')
+        def __truediv__(self, y): return x(self.v + y.v + '/')
+        def __pow__(self, y): return x(self.v + y.v + '^')
+    return eval(infix).v
+"""
+
+# Most frequently used words in a text
+
+"""
+from collections import Counter
+import re
+
+
+def top_3_words(text):
+    c = Counter(re.findall(r"[a-z']+", re.sub(r" '+ ", " ", text.lower())))
+    return [w for w,_ in c.most_common(3)]
+"""
+
+"""
+import re
+from collections import Counter
+
+top_3_words=lambda t:[w for w,c in Counter(re.findall("'*[a-z][a-z']*",t.lower())).most_common(3)]
+"""
+
+"""
+import re
+
+def top_3_words(text):
+    re_non_valid = re.compile('[^a-z\' ]')
+    re_chars = re.compile('[a-z]')
+    text = text.lower()
+    text = ' '.join([x for x in re_non_valid.sub(' ',text).split() if re_chars.search(x)])
+    words = {}
+    top = {}                    # Dictionary with no more than 3 words
+    for word in text.split():
+        if word in words:
+            words[word] += 1
+        else:
+            words[word] = 1
+        top[word] = words[word] # Add the new word to the top
+        if len(top) > 3:        # If the top is too big remove the worst word
+            top.pop(min(top, key=top.get))
+    return sorted(top, key=top.get, reverse=True)
+"""
--- warfiles/battleshipfieldvalidator.py
+++ warfiles/battleshipfieldvalidator.py
@@ -2,18 +2,12 @@
 https://www.codewars.com/kata/52bb6539a4cf1b12d90005b7/train/python
 """
 
-"""
-Option 1: Parse through linearly, one list to the next, 0-end, count any
-Catch 3 out of 10 ships
--Then flip the array, and catch 3 ships
-# Doesn't really make sense, might as well just go through it all linearly and create an alg to
-identify and continue the direction of the 1's and snake its way along until it doesn't find ones, while checking
-the zeroes around.
-"""
-
+from itertools import pairwise
 
 # Reminder: Battleship/4, 2xCruiser/3, 3xDestroyer/2, 4xSubmarine/1 Total: 20
+subbin = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1]
 
+# Flip array to get other ships
 def fliparray(array):
     array_flipped = []
     for val in range(len(field[0]) - 1):
@@ -21,45 +15,139 @@ def fliparray(array):
         for item in field:
             temp_list.append(item[val])
         array_flipped.append(temp_list)
+    print("\n")
+    for line in array_flipped:
+        print(line)
+    print("\n")
     return array_flipped
 
+# Parse array to return list of (x, y) index pairs for 1's in index
 def returnindexofones(array):
     indfield = []
-    for l in range(len(array) - 1):
-        temp_list = []
-        for item in range(len(array[l]) - 1):
+    for l in range(len(array)):
+        for item in range(len(array[l])):
             if array[l][item] == 1:
-                temp_list.append((l, item))
-        indfield.append(temp_list)
+                indfield.append((l, item))
+
     return indfield
 
-def printall(args):
-    for item in args:
-        print("\n")
-        for line in item:
-            print(line)
+# Check length of ships by indexes from 'returnindexofones'
+def returnsubtractions(arr):
+    ret = []
+    count = [0]
+    ccc = 1
+    toview = []
+    for x, y in pairwise(arr):
+        a, b = x
+        c, d = y
+        summ = (d-a) + (c-b)
+        if summ == 1:
+            ccc += 1
+            count.append(ccc)
+        else:
+            ccc = 1
+            count.append(0)
+
+        toview.append(((x, y), (a, b, c, d), (summ)))
+
+    print(toview)
+
+    print(count)
+
+    for ind in range(len(count)-1):
+        if ind < len(count):
+            if count[ind] != 0:
+                print(count[ind-1], count[ind], count[ind+1])
+                if count[ind-1] < count[ind] and count[ind+1] < count[ind]:
+                    print(f'{count[ind]}')
+                    ret.append(count[ind])
+                    print(f'RET: {ret}')
+                else:
+                    continue
+        if ind == len(count):
+            if count[ind] != 0:
+                if count[ind-1] < count[ind]:
+                    ret.append(count[ind])
+
+    return ret
+
+# Returns count of subs
+def count_sub(field):
+
+    indofones = returnindexofones(field) # List of pnts where 1's are located in matrix
+    countsub = []
+    for x, y in indofones:
+        neighbors = 0
+        surroundsound = [(x, y + 1), (x, y - 1), (x - 1, y), (x + 1, y),
+                         (x - 1, y - 1), (x + 1, y - 1), (x - 1, y - 1), (x + 1, y - 1)]
+
+        for a, b in surroundsound:
+            print(a, b)
+            if a < 0 or b < 0:
+                continue
+            elif field[a][b] == 1:
+                neighbors += 1
+
+        if neighbors == 0:
+            countsub.append(1)
+
+    return countsub
+
+def neighbors(field):
+    indofones = returnindexofones(field)  # List of pnts where 1's are located in matrix
+    neighbors = 0
+    for x, y in indofones:
+        surroundsound = [(x, y + 1), (x, y - 1), (x - 1, y), (x + 1, y),
+                         (x - 1, y - 1), (x + 1, y - 1), (x - 1, y - 1), (x + 1, y - 1)]
+
+        for a, b in surroundsound:
+            if a < 0 or b < 0:
+                continue
+            elif field[a][b] == 1:
+                neighbors += 1
+    print(f'NEIGHBORS: {neighbors}')
+    return neighbors
 
 
-def validate_battlefield(field):
-    # Check the field
+# Sinks the ships
+def sinkships(loads):
+    for payload in loads:
+        for bomb in payload:
+            try:
+                subbin.remove(bomb)
+            except: # Ship Too Large Exception
+                return "Nope, something is wrong!"
+
+def validate_battlefield(arr):
+    # Check the field for total 1 count
     t = 0
-    for y in field:
-        for x in y:
-            t += x
+    for a in field:
+        for b in a:
+            t += b
     if t != 20:
         return "Nope, something is wrong!"
-    count = 0
 
-    pnts = {}
-    indofones = returnindexofones(field)
-    for item in indofones:
+    farray = fliparray(arr)
+    pnts_flipped = returnindexofones(farray)
+    pnts = returnindexofones(arr)
+
+    payloads = [returnsubtractions(pnts), returnsubtractions(pnts_flipped), count_sub(field)]
+
+    sinkships(payloads)
+
+    if subbin:
+        return 'Yep!, Seems alright'
+    else:
+        return 'Nope, something is wrong!'
+
+
 
 
 
 
 
 if __name__ == "__main__":
-    field = [[1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+    field =       [[1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
                    [1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
                    [1, 0, 1, 0, 1, 1, 1, 0, 1, 0],
                    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -70,49 +158,21 @@ if __name__ == "__main__":
                    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-    testarray = [[0, 0, 0],
-                 [0, 1, 0],
-                 [0, 0, 0]]
+    battleField = [[1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+                   [1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+                   [1, 0, 1, 0, 1, 1, 1, 0, 1, 0],
+                   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                   [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                   [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-    x, y = 1, 1
+    print(neighbors(field))
 
-    """
-    loop through array linearly:
-    check and mark 1's:
-    check for additional 1's in nsew direction to see if ship continues
-    check corners and all the way around ship to make sure free of other ships
-    """
+    validate_battlefield(field)
 
-    def testaround(array: list, mark: tuple) -> bool:
-        x, y = mark
-        testequation = [(x - 1, y - 1), (x, y - 1), (x + 1, y - 1),
-                        (x - 1, y), (x, y), (x + 1, y),
-                        (x - 1, y - 1), (x, y - 1), (x + 1, y - 1)]
-
-        corners = [0, 2, 6, 8]
-        nsew = {'N': 1, 'E': 3, 'S': 5, 'W': 7}
-        for ind in corners:
-            a, b = testequation[ind]
-            if array[a][b] == 1:
-                return False
-        for direction in nsew:
-
-    testaround(testarray, (1, 1))
-
-
-
-    mark = (1, 1)
-    # check from x-1 to x+1
-
-
-    testarray2 = [[0, 0, 0],
-                  [0, 1, 0],
-                  [0, 1, 0]]
-
-    arrayflipped = fliparray(field)
-    indfield = returnindexofones(field)
-    indarrayflipped = returnindexofones(arrayflipped)
-    printall([field, arrayflipped, indfield, indarrayflipped])
 
 
 
